e7364b3d29681ea5b9ab604d38d0aeb8b180a899
Tomas Maurente <maurentetomas@gmail.com>
2024-07-26 18:05:31 -0300

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
@@ -12,9 +11,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.0.2")
+        classpath ("com.android.tools.build:gradle:7.0.4")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
-
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
